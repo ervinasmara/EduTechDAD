f41ed37de37499f189cd70d918c97fb8e75ac478
ervinasmara <asmaraervin@gmail.com>
2024-05-28 10:20:16 +0700

    Token 2 Days

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -32,7 +32,7 @@ public class TokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddDays(100),
+            Expires = DateTime.UtcNow.AddDays(2),
             SigningCredentials = creds
         };
 
@@ -67,7 +67,7 @@ public class TokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddDays(100),
+            Expires = DateTime.UtcNow.AddDays(2),
             SigningCredentials = creds
         };
 
@@ -110,7 +110,7 @@ public class TokenService
         var tokenDescriptor = new SecurityTokenDescriptor
         {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddDays(100),
+            Expires = DateTime.UtcNow.AddDays(2),
             SigningCredentials = creds
         };
 
