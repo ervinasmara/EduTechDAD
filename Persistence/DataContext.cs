eaf0b35c6c23ce3c1fffc7c07aca3015233be2da
ervinasmara <asmaraervin@gmail.com>
2024-06-02 11:23:09 +0700

    Add Comment in DataContext

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -20,61 +20,61 @@ public class DataContext : IdentityDbContext<AppUser>
 
         // One To Many, 1 ClassRoom have many Lesson
         modelBuilder.Entity<Lesson>()
-            .HasOne(l => l.ClassRoom)  // Satu pelajaran memiliki satu kelas
-            .WithMany(t => t.Lessons)  // Satu kelas dapat dipunyai banyak pelajaran
+            .HasOne(l => l.ClassRoom) // Satu pelajaran memiliki satu kelas
+            .WithMany(t => t.Lessons) // Satu kelas dapat dipunyai banyak pelajaran
             .HasForeignKey(l => l.ClassRoomId) // Foreign key di Lesson untuk ClassRoomId
             .OnDelete(DeleteBehavior.Restrict); // Aturan penghapusan (opsional)
 
         // One To Many, 1 ClassRoom have many Student
         modelBuilder.Entity<Student>()
-            .HasOne(s => s.ClassRoom)
-            .WithMany(c => c.Students)
-            .HasForeignKey(s => s.ClassRoomId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.ClassRoom) // Satu siswa memiliki satu kelas
+            .WithMany(c => c.Students) // Satu kelas dapat memiliki banyak siswa
+            .HasForeignKey(s => s.ClassRoomId) // Mengatur foreign key di tabel Student untuk menyimpan ID kelas
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Lesson have many Course
         modelBuilder.Entity<Course>()
-            .HasOne(s => s.Lesson)
-            .WithMany(c => c.Courses)
-            .HasForeignKey(s => s.LessonId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.Lesson) // Satu Materi memiliki satu pelajaran
+            .WithMany(c => c.Courses) // Satu pelajaran dapat memiliki banyak materi
+            .HasForeignKey(s => s.LessonId) // Mengatur foreign key di tabel Course untuk menyimpan ID pelajaran
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Course have many Assignment
         modelBuilder.Entity<Assignment>()
-            .HasOne(s => s.Course)
-            .WithMany(c => c.Assignments)
-            .HasForeignKey(s => s.CourseId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.Course) // Satu tugas memiliki satu materi
+            .WithMany(c => c.Assignments) // Satu materi dapat memiliki banyak tugas
+            .HasForeignKey(s => s.CourseId) // Mengatur foreign key di tabel Assignment untuk menyimpan ID materi
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Student have many Attendance
         modelBuilder.Entity<Attendance>()
-            .HasOne(a => a.Student)
-            .WithMany(s => s.Attendances)
-            .HasForeignKey(a => a.StudentId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(a => a.Student) // Satu kehadiran dimiliki oleh satu siswa
+            .WithMany(s => s.Attendances) // Satu siswa dapat memiliki banyak kehadiran
+            .HasForeignKey(a => a.StudentId) // Mengatur foreign key di tabel Attendance untuk menyimpan ID siswa
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Lesson have many Schedule
         modelBuilder.Entity<Schedule>()
-            .HasOne(s => s.Lesson)
-            .WithMany(c => c.Schedules)
-            .HasForeignKey(s => s.LessonId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.Lesson) // Satu jadwal dimiliki oleh satu pelajaran
+            .WithMany(c => c.Schedules) // Satu pelajaran dapat memiliki banyak jadwal
+            .HasForeignKey(s => s.LessonId) // Mengatur foreign key di tabel Schedule untuk menyimpan ID pelajaran
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Assignment have many AssignmentSubmission
         modelBuilder.Entity<AssignmentSubmission>()
-            .HasOne(s => s.Assignment)
-            .WithMany(a => a.AssignmentSubmissions)
-            .HasForeignKey(s => s.AssignmentId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(s => s.Assignment) // Satu pengumpulan tugas memiliki satu tugas
+            .WithMany(a => a.AssignmentSubmissions) // Satu tugas dapat memiliki banyak pengumpulan tugas
+            .HasForeignKey(s => s.AssignmentId) // Mengatur foreign key di tabel AssignmentSubmission untuk menyimpan ID tugas
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
         // One To Many, 1 Student have many AssignmentSubmission
         modelBuilder.Entity<AssignmentSubmission>()
-            .HasOne(z => z.Student)
-            .WithMany(s => s.AssignmentSubmissions)
-            .HasForeignKey(z => z.StudentId)
-            .OnDelete(DeleteBehavior.Restrict);
+            .HasOne(z => z.Student) // Satu pengumpulan tugas dimiliki oleh satu siswa
+            .WithMany(s => s.AssignmentSubmissions) // Satu siswa dapat memiliki banyak pengumpulan tugas
+            .HasForeignKey(z => z.StudentId) // Mengatur foreign key di tabel AssignmentSubmission untuk menyimpan ID siswa
+            .OnDelete(DeleteBehavior.Restrict); // Menentukan aturan untuk perilaku penghapusan (opsional)
 
-        // ======================== Many to Many ============================
+        /** ======================== Many to Many ============================ **/
         // Konfigurasi many-to-many relasi antara Teacher dan Lesson
         modelBuilder.Entity<TeacherLesson>()
             .HasKey(ccr => new { ccr.TeacherId, ccr.LessonId });
